81de511a0847bc6c6284c53117de47731ac02164
Seyit ahmet Denli <147086255+seyitdenli8@users.noreply.github.com>
2026-04-17 17:59:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end('<h1>Seyit Ahmet</h1><p>2200000000</p>');
+  res.end('<h1 style="color:#38bdf8">Seyit Ahmet</h1><p>2200000000</p>');
 }).listen(PORT, '0.0.0.0', () => console.log('Calisiyor: ' + PORT));
